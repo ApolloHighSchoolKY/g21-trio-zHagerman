900ef570af0b16b35de9188896025144fdac2ddd
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-04-07 14:27:33 +0000

    forgot to submit

--- Trio.java
+++ Trio.java
@@ -1,1 +1,38 @@
 //Write your answer here
+public class Trio implements menuItems
+{
+    String mySandwich = "";
+    String mySalad = "";
+    String myDrink = "";
+    double total;
+    public Trio(String sandwich,String salad,String drink)
+    {
+        mySandwich = sandwich;
+        mySalad = salad;
+        myDrink = drink;
+    }
+    public double getPrice(double y)
+    {
+        if(x == mySandwich)
+            x = sandwich;
+        else if(x == mySalad)
+            x = salad
+        else
+            x = drink;
+        return y;
+    }
+    public String getName(String x)
+    {
+        
+        x = sandwich;
+    else if(x == mySalad)
+        x = salad
+    else
+        x = drink;
+    return x;
+    }
+    public String toString()
+    {
+        return "" + mySandwich.getName() + "/" + mySalad.getName() + "/" + myDrink.getName() + " trio " + "and a price of " total;
+    }
+}
